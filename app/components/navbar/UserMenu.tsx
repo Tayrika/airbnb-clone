4f98611907b739840452ebd272286ad59ac9a7b9
Rika Miyata <rika13tay@gmail.com>
2023-10-16 11:55:28 +0900

    Create category and location selections

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import { signOut } from 'next-auth/react';
 import { SafeUser } from '@/app/types';
+import useRentModal from '@/app/hooks/useRentModal';
 
 interface UserMenuProps {
   currentUser?: SafeUser | null
@@ -19,17 +20,27 @@ const UserMenu: React.FC<UserMenuProps> = ({
 }) => {
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
+  const rentModal = useRentModal();
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
 
+  const onRent = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    rentModal.onOpen();
+  }, [currentUser, loginModal, rentModal]);
+
   return ( 
     <div className="relative">
       <div className="flex flex-row itemes-center gap-3">
         <div
-          onClick={toggleOpen}
+          onClick={onRent}
           className="
             hidden
             md:block
@@ -104,7 +115,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                   label="My properties"
                 />
                 <MenuItem 
-                  onClick={() => {}}
+                  onClick={rentModal.onOpen}
                   label="Airbnb my home"
                 />
                 <hr />
